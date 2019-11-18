da2667dcf55cd3ee1b8cf2adb463edb7c40d910e
Esther_YAO <740114152@qq.com>
2019-11-18 17:59:01 +0800

    完善了下拉菜单选中人名后显示至列表框中的功能，开始添加柱状图显示的脚本

--- front_end/web_components/bootstrap/vote_page/index.js
+++ front_end/web_components/bootstrap/vote_page/index.js
@@ -1,26 +1,78 @@
-$(document).ready(function(){
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('barChart'));
 
-	var lstArr = $('li').get();
-	console.log(lstArr);
+// 指定图表的配置项和数据
+var option = {
+	title: {
+		show: false,
+		text: '投票结果',
+		textAlign: 'center',
+		textVerticalAlign: 'center',
+		left: '50%',
+		top: '10%',
+	},
+	tooltip: {},
+	legend: {
+		data: ['票数'],
+		show: false,
+	},
+	xAxis: {
+		data: ["赞成", "反对", "弃权"],
+		axisLine: {
+			show: false,
+		},
+		axisTick: {
+			show: false,
+		},
+	},
+	yAxis: {
+		show: false,
+	},
+	series: [{
+		name: '票数',
+		type: 'bar',
+		data: [30,5,10],
+		barWidth: '20%',
+		animationEasing: 'linear',
+		animationDelayUpdate: function(idx) {
+			return idx * 10;
+		},		
+		animationDurationUpdate: function (idx) {
+			return idx * 3000;
+		},
+	}],
+	color: ['rgba(255,255,255,0.6)',],
+};
 
-    $('.btn').click(function(){
-        // alert("点击了哟~~");
-		$('.lst').css({display: 'block'});
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
 
-		
+
+// jQuery部分
+$(document).ready(function () {
+
+	//测试抓取//////////////////////////////////////////
+	var items = new Array();
+	$('.item').each(function (index) {
+		items[index] = $(this).html();
 	});
-	
-	//这里还是有问题
-	$('#l0').click(function(){
+	console.log(items);
+	///////////////////////////////////////////////////
+
+	$('.btn').click(function () {
+		// alert("点击了哟~~");
+		$('.lst').css({ display: 'block' });
+	});
+
+	//这个方法(指绑定事件)好棒棒~
+	$('.item').on("click", function () {
+		var detail = $(this).html();
 		$('#currentName').empty();
-		$('#currentName').append(lstArr[0].textContent);
-		
-		console.log('执行');
+		$('#currentName').append(detail);
 	});
-    
-    $('.cName').mouseleave(function(){
-        $('.lst').css({display: 'none'});
-    });
 
+	$('.lst').mouseleave(function () {
+		$(this).css({ display: 'none' });
+	});
 
 });
